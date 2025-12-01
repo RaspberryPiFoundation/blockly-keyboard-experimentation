bba363313b6e1b4cf48d202683de54c701f559b2
Ben Henning <bhenning@google.com>
2025-12-01 23:35:26 +0000

    chore: Add additional screenshot.

--- test/webdriverio/test/scroll_test.ts
+++ test/webdriverio/test/scroll_test.ts
@@ -117,6 +117,7 @@ suite('Scrolling into view', function () {
 
     // Assert new block has been scrolled into the viewport.
     await this.browser.pause(5000);
+    await this.browser.saveScreenshot(`failures/extra_snapshot_for_verification.png`);
     const blockBounds = await this.browser.execute(() => {
       const workspace = Blockly.getMainWorkspace() as Blockly.WorkspaceSvg;
       const block = workspace.getBlocksByType(
